4acf4f5c273e394890fc28958c2380bf3cb3f522
Mziyanda Joko <mziyandajoko@Mziyandas-MacBook-Pro.local>
2021-06-15 07:27:23 +0200

    adds changes

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,34 +6,72 @@ import 'package:madolo_217006168/widgets/topbanner.dart';
 import 'package:madolo_217006168/widgets/weathercard.dart';
 import 'package:provider/provider.dart';
 
-class MainPage extends StatelessWidget {
-  const MainPage({Key key}) : super(key: key);
+// class MainPage extends StatelessWidget {
+//   const MainPage({Key key,}) : super(key: key);
+//   final bool isLoading = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => WeatherData(),
+//       builder: (context, child) {
+//         return  Scaffold(
+//           body:
 
+//          isLoading ? Column (
+//              mainAxisAlignment: MainAxisAlignment.start,
+//             children: [ CircularProgressIndicator()]) :  Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               const TopBanner(),
+//               CurrentDayWidget() ,
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               const AppLabel(),
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               Expanded(
+//                 child: WeatherCard(),
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+class MainPage extends StatelessWidget {
+  const MainPage({
+    Key key,
+  }) : super(key: key);
+  final bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => WeatherData(),
-      builder: (context, child) {
-        return Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const TopBanner(),
-              CurrentDayWidget(),
-              SizedBox(
-                height: 10,
-              ),
-              const AppLabel(),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: WeatherCard(),
-              ),
-            ],
-          ),
-        );
-      },
-    );
+        create: (context) => WeatherData(),
+        child: Consumer<WeatherData>(
+          builder: (context, value, child) {
+            return Scaffold(
+                body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const TopBanner(),
+                CurrentDayWidget(),
+                SizedBox(
+                  height: 10,
+                ),
+                const AppLabel(),
+                SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: WeatherCard(),
+                ),
+              ],
+            ));
+          },
+        ));
   }
 }
